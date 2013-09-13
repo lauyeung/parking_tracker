cc3a9576b1346a36d09b997ec6d03c1bcddfb8b8
Louise Au Yeung <louise@eabx.org>
2013-09-13 08:17:44 -0400

    initcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParkingTracker::Application.config.secret_key_base = '71d61bded3af02b3604bd884dd10acdc05d23f002166d98156d351dabbd1bb669f3a4bc62df8a59a0f0890872702572635a689dcdb80cd34c1ec2c8293deed3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ParkingTracker::Application.config.session_store :cookie_store, key: '_parking_tracker_session'
